3b8e5d8ff83e92d8e192454ad2253d284fde2b8e
Arthur Vernon <avernon@iamin.net>
2014-08-17 08:29:23 +0800

    Document

--- src/main/webapp/scripts/vessel/service_registration.js
+++ src/main/webapp/scripts/vessel/service_registration.js
@@ -1,5 +1,7 @@
 'use strict';
-
+/*
+ * CRUD for vessel registration objects.
+ */
 geodseaApp.factory('VesselRegistration', ['$resource',
     function ($resource) {
         return $resource('app/rest/registrations/:number', {}, {
