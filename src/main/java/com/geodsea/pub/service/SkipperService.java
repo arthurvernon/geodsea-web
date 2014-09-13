3c4d9596d882585f85a2db66eba2dcf8bd7f8e66
Arthur Vernon <avernon@iamin.net>
2014-09-13 13:27:08 +0800

    implement methods to manage a trip.

--- src/main/java/com/geodsea/pub/service/SkipperService.java
+++ src/main/java/com/geodsea/pub/service/SkipperService.java
@@ -1,14 +1,264 @@
 package com.geodsea.pub.service;
 
+import com.geodsea.pub.domain.*;
+import com.geodsea.pub.repository.*;
+import com.geodsea.pub.service.util.TripSubmitChecks;
+import com.vividsolutions.jts.geom.MultiPoint;
+import com.vividsolutions.jts.geom.Point;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 import org.springframework.stereotype.Service;
 import org.springframework.transaction.annotation.Transactional;
 
+import javax.inject.Inject;
+import javax.validation.ConstraintViolation;
+import javax.validation.ConstraintViolationException;
+import javax.validation.Validator;
+import java.util.Date;
+import java.util.List;
+import java.util.Set;
+
 @Service
 @Transactional
 public class SkipperService extends BaseService {
 
+    @Inject
+    Validator validator;
+
+    @Inject
+    private TripRepository tripRepository;
+
+    @Inject
+    private SkipperRepository skipperRepository;
+
+    @Inject
+    private VesselRepository vesselRepository;
+
+    @Inject
+    private RescueRepository rescueRepository;
+
+    @Inject
+    private MonitorRepository monitorRepository;
+
+//    @Inject
+//    private LocationTimeRepository locationTimeRepository;
+
     private final Logger log = LoggerFactory.getLogger(SkipperService.class);
+
+    public Trip addTripPlan(Trip trip) {
+        return tripRepository.save(trip);
+    }
+
+    /**
+     * @param vesselId  the ID of the vessel being taken on the trip.
+     * @param skipperId the Id of the skipper in charge of the vessel
+     * @param headline  a summary of the purpose of the trip, shared with SRO
+     * @param startTime planned start time for the trip
+     * @param endTime   planned end time for the trip
+     * @param summary   a personal summary of the trip, not shared with SRO
+     * @param wayPoints optional way points for journey, beginning with the starting location
+     * @param fuel      number of litres of fuel
+     * @param people    number of people on board
+     * @return a newly created trip
+     */
+    public Trip createTripPlan(long vesselId, long skipperId, String headline, Date startTime, Date endTime, String summary, MultiPoint wayPoints, int fuel, int people) {
+        Skipper skipper = skipperRepository.getOne(skipperId);
+        if (skipper == null)
+            throw new IllegalArgumentException("No skipper with ID: " + skipperId);
+
+        if (!skipper.getPerson().isEnabled()) {
+            String msg = "Skipper's user account: " + skipper.getPerson().getParticipantName() + " is disabled";
+            log.warn(msg);
+            throw new IllegalStateException(msg);
+        }
+
+        Vessel vessel = vesselRepository.findOne(vesselId);
+        if (skipper == null)
+            throw new IllegalArgumentException("No vessel with ID: " + vesselId);
+
+        Trip trip = new Trip(vessel, skipper, headline, startTime, endTime, summary, wayPoints, fuel, people);
+
+        Set<ConstraintViolation<Trip>> constraintViolations = validator.validate( trip , TripSubmitChecks.class);
+
+        if (constraintViolations.size() > 0) {
+            for (ConstraintViolation<Trip> cv : constraintViolations)
+                log.debug(cv.getMessage());
+            throw new ConstraintViolationException(constraintViolations);
+        }
+
+        trip = tripRepository.save(trip);
+
+        monitorRepository.save(new Monitor(trip, trip.getSkipper().getPerson()));
+
+        // if way points are defined then we can include the Monitor
+        if (wayPoints != null && ! wayPoints.isEmpty()) {
+            Point point = (Point) wayPoints.getGeometryN(0);
+            establishSRO(trip, point);
+        }
+
+        // allow the skipper to monitor the trip
+        return trip;
+    }
+
+    /**
+     * Setup a sea rescue organisation based upon the location reported.
+     * @param trip the trip to monitor
+     * @param point the location to attempt
+     */
+    private void establishSRO(Trip trip, Point point)
+    {
+        List<Rescue> sroList = rescueRepository.getRescueOrganisationsForLocation(point);
+
+        // just take the first if more than one
+        if (sroList.size() > 0)
+        {
+            Rescue firstSRO = sroList.get(0);
+
+            trip.setRescue(firstSRO);
+
+            tripRepository.save(trip);
+
+            // Set up a monitor that has no expiry date.
+            // it will expire on completion of the trip.
+            Monitor monitor = new Monitor(trip, firstSRO.getOrgansation());
+            monitorRepository.save(monitor);
+        }
+        else
+            log.info("No sea rescue organisation covers " + point);
+    }
+
+    public Trip getTrip(long id) {
+        return tripRepository.findOne(id);
+    }
+
+    /**
+     * Notification that the specified trip has commenced.
+     * <p>
+     * Without a geographical location
+     * </p>
+     *
+     * @param tripId  ID of an existing trip.
+     * @param started optional start date/time. If not specified the trip will be assumed to have started now
+     */
+    public void commenceTrip(long tripId, Date started) {
+        Trip trip = tripRepository.findOne(tripId);
+
+        if (trip == null)
+            throw new IllegalArgumentException("No such trip: " + tripId);
+
+        trip.setActualStartTime(new Date());
+        tripRepository.save(trip);
+    }
+
+    public void reportLocation(long tripId, LocationTime locationTime) {
+        if (locationTime == null)
+            return;
+
+        Trip trip = tripRepository.findOne(tripId);
+
+        if (trip == null)
+            throw new IllegalArgumentException("No such trip: " + tripId);
+
+        trip.add(locationTime);
+//        locationTimeRepository.save(locationTime);
+        tripRepository.save(trip);
+    }
+
+    public void reportLocations(long tripId, List<LocationTime> locationTimes) {
+        if (locationTimes == null || locationTimes.size() == 0)
+            return;
+
+        Trip trip = tripRepository.findOne(tripId);
+
+        if (trip == null)
+            throw new IllegalArgumentException("No such trip: " + tripId);
+
+        trip.addAll(locationTimes);
+        tripRepository.save(trip);
+
+    }
+
+    public void deleteTrip(long tripId) {
+        Trip trip = tripRepository.findOne(tripId);
+
+        if (trip == null)
+            throw new IllegalArgumentException("No such trip: " + tripId);
+
+        tripRepository.delete(trip);
+    }
+
+    public Skipper getOrCreateSkipper(String username) {
+
+        Person person = personRepository.getUserByParticipantName(username);
+        if (person == null) {
+            throw new IllegalArgumentException("No such user: " + username);
+        }
+        if (! person.isEnabled())
+            throw new IllegalArgumentException("Disabled user: " + username);
+
+        Skipper skipper = skipperRepository.findByPersonParticipantName(username);
+        if (skipper != null)
+            log.debug("User: " + username + " is already a skipper");
+        else {
+            log.debug("creating a skipper for User: " + username);
+            skipper = new Skipper(person);
+            skipperRepository.save(skipper);
+        }
+        return skipper;
+    }
+
+    public void removeSkipper(long skipperId) {
+        Skipper skipper = skipperRepository.getOne(skipperId);
+
+        if (skipper != null)
+        {
+            log.debug("Deleting skipper record for User: " + skipper.getPerson().getParticipantName());
+            skipperRepository.delete(skipper);
+        }
+        else
+            log.debug("No such skipper: " + skipperId);
+    }
+
+    /**
+     * Get the track history for this trip.
+     * @param tripId
+     * @return
+     */
+    @Transactional(rollbackFor = ActionRefusedException.class)
+    public List<LocationTime> getLocationTimes(String user, long tripId) throws ActionRefusedException {
+        Trip trip = lookupTripAndCheckMonitor(tripId, user);
+        return trip.getLocationTimes();
+    }
+
+
+    /**
+     * Get the latest reported location (if any) in relation to the trip.
+     * @param tripId
+     * @return
+     */
+    @Transactional(rollbackFor = ActionRefusedException.class)
+    public LocationTime getLatestLocationTime(String user, long tripId) throws ActionRefusedException {
+        Trip trip = lookupTripAndCheckMonitor(tripId, user);
+        final List<LocationTime> locations = trip.getLocationTimes();
+        if (locations == null || locations.size() == 0)
+            return null;
+        else
+            return locations.get(locations.size()-1);
+    }
+
+    private Trip lookupTripAndCheckMonitor(long tripId, String participant) throws ActionRefusedException {
+        Trip trip = tripRepository.findOne(tripId);
+
+        if (trip == null)
+            throw new IllegalArgumentException("No such trip: " + tripId);
+
+        Monitor monitor = monitorRepository.findByParticipantParticipantName(participant);
+        if (monitor == null)
+        {
+            log.warn("User: {} not permitted to access trip: {}", participant, tripId);
+            throw new ActionRefusedException("Participant" + participant + " is not permitted to access Trip: " + tripId);
+        }
+        return trip;
+    }
 
 }
---
+++ src/test/java/com/geodsea/pub/service/SkipperServiceTripTest.java
@@ -0,0 +1,157 @@
+package com.geodsea.pub.service;
+
+import com.geodsea.pub.Application;
+import com.geodsea.pub.domain.*;
+import com.geodsea.pub.domain.type.VesselType;
+import com.geodsea.pub.domain.util.DateConstants;
+import com.geodsea.pub.repository.*;
+import com.vividsolutions.jts.geom.Point;
+import org.apache.log4j.Logger;
+import org.junit.After;
+import org.junit.Before;
+import org.junit.Test;
+import org.junit.runner.RunWith;
+import org.springframework.boot.test.SpringApplicationConfiguration;
+import org.springframework.test.context.ActiveProfiles;
+import org.springframework.test.context.junit4.SpringJUnit4ClassRunner;
+import org.springframework.test.context.web.WebAppConfiguration;
+
+import javax.inject.Inject;
+
+import java.util.ArrayList;
+import java.util.Date;
+import java.util.List;
+
+import static org.assertj.core.api.Assertions.assertThat;
+
+/**
+ * Test class for the UserResource REST controller.
+ *
+ * @see com.geodsea.pub.service.UserService
+ */
+@RunWith(SpringJUnit4ClassRunner.class)
+@SpringApplicationConfiguration(classes = Application.class)
+@WebAppConfiguration
+@ActiveProfiles("dev")
+//@Transactional()
+public class SkipperServiceTripTest {
+
+    private static final Logger logger = Logger.getLogger(SkipperServiceTripTest.class);
+
+    @Inject
+    private GisService gisService;
+
+    @Inject
+    private SkipperService skipperService;
+
+    @Inject
+    private VesselRepository vesselRepository;
+
+    @Inject
+    private SkipperRepository skipperRepository;
+
+    @Inject
+    private PersonRepository personRepository;
+
+    private Vessel vessel;
+
+    private Skipper skipper;
+
+    @Before
+    public void setup() {
+        vessel = new Vessel("Monty", VesselType.CABIN);
+        vessel = vesselRepository.save(vessel);
+        skipper = skipperService.getOrCreateSkipper("user");
+    }
+
+    @After
+    public void cleanup() {
+        logger.debug("Before deleting vessel");
+        if (vessel.getId() != null)
+            vesselRepository.delete(vessel);
+        logger.debug("After deleting vessel");
+        logger.debug("Before removing skipper");
+        skipperService.removeSkipper(skipper.getId());
+        logger.debug("After removing skipper");
+    }
+
+    @Test
+    public void testTripLifecycle() throws ActionRefusedException {
+
+//        int locationReportsBefore = locationTimeRepository.findAll().size();
+
+        // need to delete this once done!
+
+
+        long now = System.currentTimeMillis();
+
+        long currentTime = now + 1 * DateConstants.DAYS;
+        long finish = now + 1 * DateConstants.DAYS + 1 * DateConstants.HOURS;
+
+        // when a plan is made, the trip is defined.
+        // vesselId, skipperId, headline, startTime, endTime, summary, MultiPoint wayPoints, int fuel, int people
+        Trip trip = skipperService.createTripPlan(vessel.getId(), skipper.getId(), "Test", new Date(currentTime), new Date(finish), "Summary", null, 100, 3);
+
+        // the person then makes a start
+        trip = skipperService.getTrip(trip.getId());
+
+        // actual start 30 seconds later than planned start
+        currentTime = currentTime +  + 30000;
+        skipperService.commenceTrip(trip.getId(), new Date(currentTime));
+
+
+        // test data for starting location - Hillary's boat ramp 10 minutes ago
+        double latitude = GisService.googleLat(" 31°49'17.72\"S");
+        double longitude = GisService.googleLong("115°44'20.36\"E");
+        Point point = gisService.createPointFromLatLong(latitude, longitude);
+
+        // first report one minute later, once GPS has settled.
+        currentTime = currentTime + 1 * DateConstants.MINUTES;
+        Date locationReportTime = new Date(currentTime);
+        LocationTime location = new LocationTime(point, locationReportTime, locationReportTime);
+
+        skipperService.reportLocation(trip.getId(), location);
+
+        // some time later a location report is received.
+        // and the location reports appended.
+        List<LocationTime> updates = exitHillarys(currentTime);
+        int updateSize = updates.size();
+        skipperService.reportLocations(trip.getId(), updates);
+
+        Trip currentTrip = skipperService.getTrip(trip.getId());
+
+        List<LocationTime> locationTimes = skipperService.getLocationTimes("user", trip.getId());
+
+        // check
+        assertThat(locationTimes.size()).isEqualTo(1 + updateSize);
+
+
+        skipperService.deleteTrip(trip.getId());
+
+        // person and skipper are not deleted in the process
+        assertThat(personRepository.getUserByParticipantName("user")).isNotNull();
+        assertThat(skipperRepository.getOne(skipper.getId())).isNotNull();
+    }
+
+    private List<LocationTime> exitHillarys(long start) {
+        List<LocationTime> list = new ArrayList<LocationTime>();
+
+        long afterLastLocationReport = start + 7 * DateConstants.MINUTES;
+        list.add(create("31°49'24.25\"S", "115°44'19.58\"E", start += DateConstants.MINUTES, afterLastLocationReport));
+        list.add(create("31°49'27.13\"S", "115°44'0.40\"E", start += DateConstants.MINUTES, afterLastLocationReport));
+        list.add(create(" 31°49'12.11\"S", "115°43'54.81\"E", start += DateConstants.MINUTES, afterLastLocationReport));
+        list.add(create(" 31°48'41.91\"S", "115°43'35.84\"E", start += DateConstants.MINUTES, afterLastLocationReport));
+        list.add(create(" 31°48'27.17\"S", "115°43'28.91\"E", start += DateConstants.MINUTES, afterLastLocationReport));
+        list.add(create(" 31°48'1.64\"S", "115°43'26.96\"E", start += DateConstants.MINUTES, afterLastLocationReport));
+
+        return list;
+    }
+
+    private LocationTime create(String lat, String lon, long at, long received) {
+        double latitude = GisService.googleLat(lat);
+        double longitude = GisService.googleLong(lon);
+        Point point = gisService.createPointFromLatLong(latitude, longitude);
+        LocationTime location = new LocationTime(point, new Date(at), new Date(received));
+        return location;
+    }
+}
