3779efcbdf02496a9a56cce0ffc86cf0c7aa17ab
Arthur Vernon <avernon@iamin.net>
2014-09-17 19:50:42 +0800

    Verify behaviour of geo query.

---
+++ src/test/java/com/geodsea/pub/service/LicensorTest.java
@@ -0,0 +1,58 @@
+package com.geodsea.pub.service;
+
+import com.geodsea.pub.Application;
+import com.geodsea.pub.domain.Group;
+import com.geodsea.pub.domain.Licensor;
+import com.geodsea.pub.repository.LicensorRepository;
+import com.vividsolutions.jts.geom.Point;
+import org.apache.log4j.Logger;
+import org.junit.Test;
+import org.junit.runner.RunWith;
+import org.springframework.boot.test.SpringApplicationConfiguration;
+import org.springframework.security.authentication.UsernamePasswordAuthenticationToken;
+import org.springframework.security.core.context.SecurityContextHolder;
+import org.springframework.test.context.ActiveProfiles;
+import org.springframework.test.context.junit4.SpringJUnit4ClassRunner;
+import org.springframework.test.context.web.WebAppConfiguration;
+
+import javax.inject.Inject;
+
+import java.util.List;
+
+import static org.assertj.core.api.Assertions.assertThat;
+
+/**
+ * Test class for the UserResource REST controller.
+ *
+ * @see com.geodsea.pub.service.UserService
+ */
+@RunWith(SpringJUnit4ClassRunner.class)
+@SpringApplicationConfiguration(classes = Application.class)
+@WebAppConfiguration
+@ActiveProfiles("dev")
+public class LicensorTest {
+
+    private static final Logger logger = Logger.getLogger(LicensorTest.class);
+
+
+    @Inject
+    LicensorRepository licensorRepository;
+    @Inject
+    GisService gisService;
+
+    @Test
+    public void testMemberQuery() throws ActionRefusedException {
+
+        Point point = gisService.createPointFromLatLong(-32, 115);
+        List<Licensor> licensor = licensorRepository.getLicensorForLocation(point);
+
+        assertThat(licensor.size()).isEqualTo(1);
+
+        // Perth in the nothern hemishpere where there is no licensor
+        point = gisService.createPointFromLatLong(32, 115);
+        licensor = licensorRepository.getLicensorForLocation(point);
+        assertThat(licensor.size()).isEqualTo(0);
+
+    }
+
+}
