3d6a3ca3b30547ff1b05eb675acfafb612d43d83
Arthur Vernon <avernon@iamin.net>
2014-09-29 14:45:27 +0800

    Map working

--- src/main/webapp/scripts/trip/controller_trip.js
+++ src/main/webapp/scripts/trip/controller_trip.js
@@ -5,65 +5,113 @@ geodseaApp.controller('TripController', ['$scope', 'resolvedTrip', 'Trip', '$tim
 
         $scope.trips = resolvedTrip;
         $scope.map = {};
+        $scope.linestring = null;
         $scope.map.draw = null;
         $scope.map.source = new ol.source.Vector();
         $scope.map.raster = new ol.layer.Tile({
             source: new ol.source.OSM()
         });
-        $scope.map.vector = new ol.layer.Vector({
-            source: $scope.map.source,
-            style: new ol.style.Style({
-                fill: new ol.style.Fill({
-                    color: 'rgba(255, 255, 255, 0.2)'
-                }),
-                stroke: new ol.style.Stroke({
-                    color: '#ffcc33',
-                    width: 2
-                }),
-                image: new ol.style.Circle({
-                    radius: 7,
-                    fill: new ol.style.Fill({
-                        color: '#ffcc33'
-                    })
-                })
-            })
-        });
 
-//        $scope.interact = function () {
-//            if ($scope.draw != null)
-//                $scope.map.removeInteraction($scope.draw);
-//            if ($scope.geotype !== 'None') {
-//                $scope.draw = new ol.interaction.Draw({
-//                    source: $scope.source,
-//                    type: /** @type {ol.geom.GeometryType} */ 'LineString'
-//                });
-//                $scope.map.addInteraction($scope.draw);
-//            }
-//        }
-
-
-        $scope.initmap = function(){
+        $scope.initmap = function () {
 
             // timeout required so that the containing div is rendered and gains a height and width
-            $timeout(function(){
+            $timeout(function () {
                 if ($scope.map.map == undefined) {
                     $scope.map.map = new ol.Map({
-                        layers: [$scope.map.raster, $scope.map.vector],
+                        layers: [$scope.map.raster],
+//                        layers: [$scope.map.raster, $scope.map.vector],
                         target: 'map',
+                        controls: ol.control.defaults().extend([
+                            new ol.control.ScaleLine(), new ol.control.FullScreen()
+                        ]),
                         view: new ol.View({
-                            center: [-11000000, 4600000],
-                            zoom: 4
+                            projection: 'EPSG:3857',
+                            center: ol.proj.transform([115.44, -31.98], 'EPSG:4326', 'EPSG:3857'),
+//                            center: [-11000000, 4600000],
+                            zoom: 10
                         })
                     });
+
+                    // The features are not added to a regular vector layer/source,
+                    // but to a feature overlay which holds a collection of features.
+                    // This collection is passed to the modify and also the draw
+                    // interaction, so that both can add or modify features.
+                    $scope.map.featureOverlay = new ol.FeatureOverlay({
+                        style: new ol.style.Style({
+                            fill: new ol.style.Fill({
+                                color: 'rgba(255, 255, 255, 0.2)'
+                            }),
+                            stroke: new ol.style.Stroke({
+                                color: '#ffcc33',
+                                width: 2
+                            }),
+                            image: new ol.style.Circle({
+                                radius: 7,
+                                fill: new ol.style.Fill({
+                                    color: '#ffcc33'
+                                })
+                            })
+                        })
+                    });
+                    $scope.map.featureOverlay.setMap($scope.map.map);
+
+                    $scope.map.modify = new ol.interaction.Modify({
+                        features: $scope.map.featureOverlay.getFeatures(),
+                        // the SHIFT key must be pressed to delete vertices, so
+                        // that new vertices can be drawn at the same position
+                        // of existing vertices
+                        deleteCondition: function (event) {
+                            return ol.events.condition.shiftKeyOnly(event) &&
+                                ol.events.condition.singleClick(event);
+                        }
+                    });
+                    $scope.map.map.addInteraction($scope.map.modify);
+
                     $scope.map.draw = new ol.interaction.Draw({
+                        features: $scope.map.featureOverlay.getFeatures(),
                         source: $scope.map.source,
                         type: /** @type {ol.geom.GeometryType} */ 'LineString'
                     });
                     $scope.map.map.addInteraction($scope.map.draw);
 
-                }
-            },200,false);
+                    $scope.map.jsonformat = new ol.format.GeoJSON;
 
+                    $scope.map.draw.on('drawstart',
+                        function (evt) {
+                            $scope.map.featureOverlay.getFeatures().clear();
+                            var feature = evt.feature;
+                            feature.on('change', function (e) {
+                                $scope.maintainFeatureValue(e.currentTarget);
+                            })
+                        });
+                    $scope.map.draw.on('drawend',
+                        function (evt) {
+                            $scope.maintainFeatureValue(evt.feature);
+                        }, this);
+
+                }
+            }, 200, false);
+
+        };
+
+        /*
+         * Maintain the linestring value as drawn on the map
+         */
+        $scope.maintainFeatureValue = function (feature) {
+            if (feature.getGeometry() instanceof ol.geom.LineString) {
+
+                $scope.map.linestring = JSON.stringify(
+                    $scope.map.jsonformat.writeFeatureObject(feature,
+                        {
+                            // the format it is to be written to
+                            dataProjection: 'EPSG:4326',
+
+                            // the format the information is in
+                            featureProjection: 'EPSG:3857'
+                        })
+                );
+                $scope.$apply();
+            }
         };
 
         $scope.create = function () {
@@ -92,6 +140,7 @@ geodseaApp.controller('TripController', ['$scope', 'resolvedTrip', 'Trip', '$tim
             $scope.trip = {id: null, sampleTextAttribute: null, sampleDateAttribute: null};
             $scope.initmap();
         };
+
     }]);
 
 
