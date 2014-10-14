adce8348201d641416db1425f46fb821f630ab61
Arthur Vernon <avernon@iamin.net>
2014-10-15 05:54:53 +0800

    Web service API changes

--- src/main/webapp/scripts/vessel/service_vessel.js
+++ src/main/webapp/scripts/vessel/service_vessel.js
@@ -1,5 +1,8 @@
 'use strict';
 
+/**
+ * All the vessels or that vessel with the specified vessel ID
+ */
 geodseaApp.factory('Vessel', ['$resource',
     function ($resource) {
         return $resource('app/rest/vessels/:id', {}, {
@@ -8,9 +11,23 @@ geodseaApp.factory('Vessel', ['$resource',
         });
     }]);
 
+/*
+All the skippers for a specified vessel ID.
+ */
 geodseaApp.factory('VesselSkipper', ['$resource',
     function ($resource) {
         return $resource('app/rest/vessel/:vesselId/skippers', {}, {
+            'query': { method: 'GET', isArray: true}
+        });
+    }]);
+
+
+/*
+ All the skippers of the vessel being used on a skipper trip.
+ */
+geodseaApp.factory('TripVesselSkipper', ['$resource',
+    function ($resource) {
+        return $resource('app/rest/trips/:tripId/vessel/skippers', {}, {
             'query': { method: 'GET', isArray: true}
         });
     }]);
