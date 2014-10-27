c85cb9db14515e1926f3ffdf60d09909015de98d
Arthur Vernon <avernon@iamin.net>
2014-10-27 20:11:10 +0800

    no checking on questions

--- src/main/java/com/geodsea/pub/config/SecurityConfiguration.java
+++ src/main/java/com/geodsea/pub/config/SecurityConfiguration.java
@@ -108,6 +108,7 @@ public class SecurityConfiguration extends WebSecurityConfigurerAdapter {
                 .antMatchers("/app/rest/register").permitAll()
                 .antMatchers("/app/rest/activate").permitAll()
                 .antMatchers("/app/rest/authenticate").permitAll()
+                .antMatchers("/app/rest/question/**").permitAll()
                 .antMatchers("/app/rest/logs/**").hasAuthority(AuthoritiesConstants.ADMIN)
                 .antMatchers("/app/**").authenticated()
                 .antMatchers("/websocket/tracker").hasAuthority(AuthoritiesConstants.ADMIN)
